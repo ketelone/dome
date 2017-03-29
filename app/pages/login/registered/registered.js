1b56340f350e7f8af4019cd4e9fabc6fb32c029f
chenjiacheng <jiacheng.chen@hand-china.com>
2017-03-29 10:13:17 +0800

    1

---
+++ app/pages/login/registered/registered.js
@@ -0,0 +1,34 @@
+/**
+ * Created by daidongdong on 17/3/28.
+ */
+angular.module('loginModule')
+
+  .controller('registeredCtrl', [
+    '$scope',
+    '$state',
+    'baseConfig',
+    '$ionicLoading',
+    '$http',
+    '$timeout',
+    '$ionicHistory',
+    '$ionicPlatform',
+    '$ionicScrollDelegate',
+    'hmsPopup',
+    '$rootScope',
+    'publicMethod',
+    function ($scope,
+              $state,
+              baseConfig,
+              $ionicLoading,
+              $http,
+              $timeout,
+              $ionicHistory,
+              $ionicPlatform,
+              $ionicScrollDelegate,
+              hmsPopup,
+              $rootScope, publicMethod) {
+
+
+
+
+    }]);
