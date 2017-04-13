4040cfd93e6d8bc0f090bd3893070c10e0e7435b
handhand <dongdong.dai@hand-china.com>
2017-04-13 18:11:37 +0800

    leaving morning

--- app/pages/keyscene-morning/morningCtrl.js
+++ app/pages/keyscene-morning/morningCtrl.js
@@ -5,16 +5,28 @@ angular.module('productModule')
   .controller('morningCtrl', [
     '$scope',
     '$state',
-    'publicMethod', '$ionicModal', '$ionicPopover', '$timeout', '$ionicHistory', 'hmsHttp','hmsPopup',
-    function ($scope, $state, publicMethod, $ionicModal, $ionicPopover, $timeout, $ionicHistory, hmsHttp,hmsPopup) {
+    'publicMethod', '$ionicModal', '$ionicPopover', '$timeout', '$ionicHistory', 'hmsHttp', 'hmsPopup',
+    function ($scope, $state, publicMethod, $ionicModal, $ionicPopover, $timeout, $ionicHistory, hmsHttp, hmsPopup) {
 
 
       $scope.config = {
-        openFlag : true,
-        device1 : false,
-        device2 : false,
-        device3 : false,
-        device4 : false
+        openFlag: true,
+        device1: false,//shebei
+        device2: false,
+        device3: false,
+        device4: false,
+        flagDevice1: false,//shifouanzhuang
+        flagDevice2: false,
+        flagDevice3: false,
+        flagDevice4: false,
+        onOrOff1 : true,//shifouzaixian
+        onOrOff2 : true,
+        onOrOff3 : true,
+        onOrOff4 : true,
+        onLinePic1 : "build/img/keyscene-morning/icon_home_device_signal5.png",//tupian
+        onLinePic2 : "build/img/keyscene-morning/icon_home_device_signal5.png",
+        onLinePic3 : "build/img/keyscene-morning/icon_home_device_signal5.png",
+        onLinePic4 : "build/img/keyscene-morning/icon_home_device_signal5.png",
       }
       /**
        *@autor:daidongdong
@@ -35,62 +47,73 @@ angular.module('productModule')
        *@disc:openKeysceneMorning
        */
       $scope.openKeyscene = function () {
-        if($scope.config.openFlag == true){
-          $("#progressAnimation").css({
-            "-webkit-animation": "aaa 1.5s linear",
-            "background": "#1a1d28"
-        });
-          $timeout(function(){
-            $scope.config.device1 = true;
-            $scope.config.openFlag = false;
-          },1400);
+        console.log($scope.config.openFlag);
+        if ($scope.config.openFlag == true) {
+          //马桶
+          if($scope.config.flagDevice1 != true){
+            console.log($scope.config.flagDevice1);
+            $("#progressAnimation1").css({
+              "-webkit-animation": "aaa 5.5s linear",
+              "background": "#1a1d28"
+            });
+            $timeout(function () {
+              $scope.config.device1 = true;
+              $scope.config.openFlag = false;
+            }, 5700);
+          }
+          //浴霸
+          if($scope.config.flagDevice2 != true) {
+            $("#progressAnimation3").css({
+              "-webkit-animation": "aaa 3.0s linear",
+              "background": "#1a1d28"
+            });
+            $timeout(function () {
+              $scope.config.device3 = true;
+            }, 2800);
+          }
+          //淋浴
+          if($scope.config.flagDevice3 != true) {
+            $("#progressAnimation2").css({
+              "-webkit-animation": "aaa 6.0s linear",
+              "background": "#1a1d28"
+            });
+            $timeout(function () {
+              $scope.config.device2 = true;
+            }, 5900);
+          }
+          //净水
+          if($scope.config.flagDevice4 != true) {
+            $("#progressAnimation4").css({
+              "-webkit-animation": "aaa 1.5s linear",
+              "background": "#1a1d28"
+            });
+            $timeout(function () {
+              $scope.config.device4 = true;
+            }, 1600);
+          }
+        } else {
 
-          $("#progressAnimation3").css({
-            "-webkit-animation": "aaa 3.0s linear",
-            "background": "#1a1d28"
-          });
-          $timeout(function(){
-            $scope.config.device3 = true;
-          },2800);
-        }else{
-          $("#progressAnimation2").css({
-            "-webkit-animation": "aaa 6.0s linear",
-            "background": "#1a1d28"
-          });
-          $timeout(function(){
-            $scope.config.device2 = true;
-          },5900);
-
-          $("#progressAnimation4").css({
-            "-webkit-animation": "aaa 1.5s linear",
-            "background": "#1a1d28"
-          });
-          $timeout(function(){
-            $scope.config.device4 = true;
-          },1400);
         }
       }
 
       /**
-      *@autor:daidongdong
-      *@name:deleteKeyscene
-      *@params:
-      *@return:
-      *@disc:delete Keyscene
-      */
-      $scope.deleteKeyscene = function(){
-        hmsPopup.confirmNoTitle('删除场景',deleteKey);
-        function deleteKey(){
+       *@autor:daidongdong
+       *@name:deleteKeyscene
+       *@params:
+       *@return:
+       *@disc:delete Keyscene
+       */
+      $scope.deleteKeyscene = function () {
+        hmsPopup.confirmNoTitle('删除场景', deleteKey);
+        function deleteKey() {
           var url = "";
-          var paramter = {
-
-          }
+          var paramter = {}
           hmsHttp.post(url, paramter).success(
-            function(response){
+            function (response) {
 
             }
           ).error(
-            function (response, status, header, config){
+            function (response, status, header, config) {
             }
           );
         }
--- app/pages/test/test.js
+++ app/pages/test/test.js
@@ -57,7 +57,7 @@ angular.module('loginModule')
 
 
       $scope.listData = ['zhe1', 'zhe2', 'zhe3', 'zhe4', 'zhe5', 'zhe6', 'zhe7', 'zhe8', 'zhe9',
-        'zhe10', 'zh11', 'zhe12', 'zhe13', 'zhe14', 'zhe15', 'zhe16', 'zhe17', 'zhe18'];
+        'zhe10', 'zh11', 'zhe12', 'zhe13', 'zhe14', 'zhe15', 'zhe16', 'zhe17', 'zhe18','zhe19','zhe20','zhe21','zhe22','zhe23','zhe24'];
 
 
       //滚动啊
@@ -90,10 +90,12 @@ angular.module('loginModule')
 
       scope.lastDeg = 0;
       scope.onDragScroll = function ($event) {
-        if (scope.weeks[scope.nowIndex-1] <= min && $event.gesture.deltaY > 0) {
+        // console.log(scope.nowIndex);
+        // console.log(scope.weeks[scope.nowIndex]);
+        if (scope.weeks[scope.nowIndex] <= min && $event.gesture.deltaY > 0) {
           return;
         }
-        if (scope.weeks[scope.nowIndex-1] >= max && $event.gesture.deltaY < 0) {
+        if (scope.weeks[scope.nowIndex] >= max && $event.gesture.deltaY < 0) {
           return;
         }
         scope.lastDeg = scope.sourceDeg - $event.gesture.deltaY / 2;
@@ -118,7 +120,7 @@ angular.module('loginModule')
 
 
       var min = 1;
-      var max = 18;
+      var max = 24;
       function getNextIndex(nowIndex) {
         var temp = nowIndex + 1;
         if (temp > (weekNum - 1))
@@ -164,284 +166,13 @@ angular.module('loginModule')
 
 
       scope.weeks = [];
-      console.log(scope.listData[1]);
       for (var i = 0; i < scope.listData.length; i++) {
         scope.weeks.push(i + 1);
       }
 
-      scope.getShowString = function (date) {
-        return date;
-      }
       scope.getShowString = function (item) {
-        return scope.listData[item];
+        return scope.listData[item-1];
       };
 
-      scope.onReleaseYear = function ($event) {
-
-      };
 
     }])
-
-  .directive('infinityScroll', function () {
-    return {
-      restrict: 'AE',
-      scope: {
-        type: '=type',
-        max: '=max',
-        min: '=min',
-        titleText: '=titleText',
-        listData: '=listData',
-        weekNum: '=weekNum'
-      },
-      template: '<div class="year" on-release="onReleaseYear($event)"> {{year}} </div>' +
-      '<div class="scroll-container" on-drag="onDragScroll($event)" on-release="onReleaseScroll($event)">' +
-      '<div class="stage" ng-style="stageRotate">' +
-      '<div class="scroll-item" ng-repeat="week in weeks track by $index" ng-style="rotates[$index]" ng-class="{true:\'selected\'}[nowIndex == $index]">{{getShowString(week)}}' +
-      '</div>' +
-      '</div>' +
-      '</div>' +
-      '<div ng-repeat="i in listData" ng-show="false">{{i}}</div>' +
-      '<div class="select-week" ng-click="selectItem()">确定</div>',
-      link: function (scope, element, attrs) {
-        scope.rotates = [];
-        scope.sourceDeg = 180;
-        scope.weeksIndex = [];
-        var weekNum = attrs.weekNum;
-        scope.nowIndex = weekNum / 2;
-        scope.stageRotate = {
-          "transform": "rotateX(" + scope.sourceDeg + "deg)",
-          "-webkit-transform": "rotateX(" + scope.sourceDeg + "deg)"
-        };
-        for (var i = 0; i < weekNum; i++) {
-          scope.weeksIndex.push(i);
-        }
-        scope.baseDeg = 360 / scope.weeksIndex.length;
-
-        for (var i = (weekNum - 1); i >= 0; i--) {
-          var deg = 360 / weekNum * (weekNum - i);
-          scope.rotates[i] = {
-            "transform": "rotateX(" + deg + "deg) translateZ(90px)",
-            "-webkit-transform": "rotateX(" + deg + "deg) translateZ(90px)"
-          };
-        }
-
-        function getLastDeg(deg) {
-          return (Math.round(deg / scope.baseDeg) + 1) * scope.baseDeg;
-        }
-
-        function getNextIndex(nowIndex) {
-          var temp = nowIndex + 1;
-          if (temp > (weekNum - 1))
-            temp = 0;
-          return temp;
-        }
-
-        function getPrevIndex(nowIndex) {
-          var temp = nowIndex - 1;
-          if (temp < 0)
-            temp = weekNum - 1;
-          return temp;
-        }
-
-        function getNowIndex(deg) {
-          var temp = deg / scope.baseDeg % weekNum;
-          scope.nowIndex = temp < 0 ? (weekNum + temp) : temp;
-          refreshData();
-        }
-
-        scope.lastDeg = 0;
-        scope.onDragScroll = function ($event) {
-          if (attrs.type == 'number' || attrs.type == 'dateList') {
-            if (scope.weeks[scope.nowIndex] <= min && $event.gesture.deltaY > 0) {
-              return;
-            }
-            if (scope.weeks[scope.nowIndex] >= max && $event.gesture.deltaY < 0) {
-              return;
-            }
-          }
-          scope.lastDeg = scope.sourceDeg - $event.gesture.deltaY / 2;
-          scope.stageRotate = {
-            "transform": "rotateX(" + scope.lastDeg + "deg)",
-            "-webkit-transform": "rotateX(" + scope.lastDeg + "deg)"
-          };
-          getNowIndex(scope.lastDeg);
-        };
-
-        scope.onReleaseScroll = function ($event) {
-          if ($event.gesture.deltaY > 0)
-            scope.sourceDeg = getLastDeg(scope.lastDeg) - scope.baseDeg;
-          else
-            scope.sourceDeg = getLastDeg(scope.lastDeg - scope.baseDeg);
-          scope.stageRotate = {
-            "transform": "rotateX(" + scope.sourceDeg + "deg)",
-            "-webkit-transform": "rotateX(" + scope.sourceDeg + "deg)"
-          };
-        };
-
-        if (attrs.type == 'number') {
-          scope.year = attrs.titleText ? attrs.titleText : 'title';
-          var min = attrs.min ? Number.parseInt(attrs.min) : 0;
-          var max = attrs.max ? attrs.max : 100;
-
-          function refreshData() {
-            scope.nowIndex = Math.round(scope.nowIndex);
-            if (scope.nowIndex > (weekNum - 1))
-              scope.nowIndex -= weekNum;
-
-            var needChangeUpIndex = scope.nowIndex - weekNum / 4;
-            if (needChangeUpIndex < 0)
-              needChangeUpIndex = weekNum + needChangeUpIndex;
-
-            if (scope.weeks[getNextIndex(needChangeUpIndex)] == '' || scope.weeks[getNextIndex(needChangeUpIndex)] - 1 < min)
-              scope.weeks[needChangeUpIndex] = '';
-            else
-              scope.weeks[needChangeUpIndex] = scope.weeks[getNextIndex(needChangeUpIndex)] - 1;
-
-            var needChangeDownIndex = scope.nowIndex + weekNum / 4;
-            if (needChangeDownIndex > (weekNum - 1))
-              needChangeDownIndex = needChangeDownIndex - weekNum;
-
-            if (scope.weeks[getPrevIndex(needChangeDownIndex)] == '' || scope.weeks[getPrevIndex(needChangeDownIndex)] + 1 > max)
-              scope.weeks[needChangeDownIndex] = '';
-            else
-              scope.weeks[needChangeDownIndex] = scope.weeks[getPrevIndex(needChangeDownIndex)] + 1;
-          }
-
-          scope.weeks = [];
-          for (var i = min; i < weekNum + min; i++) {
-            scope.weeks[i - min] = i;
-          }
-
-          scope.getShowString = function (item) {
-            return item;
-          };
-
-          scope.onReleaseYear = function ($event) {
-
-          };
-
-          scope.selectItem = function () {
-            alert(scope.weeks[scope.nowIndex]);
-          };
-        } else if (attrs.type == 'dateList') {
-          scope.year = attrs.titleText ? attrs.titleText : 'title';
-          var min = attrs.min ? Number.parseInt(attrs.min) : 0;
-          var max = attrs.max ? attrs.max : 100;
-
-          function refreshData() {
-            scope.nowIndex = Math.round(scope.nowIndex);
-            if (scope.nowIndex > (weekNum - 1))
-              scope.nowIndex -= weekNum;
-
-            var needChangeUpIndex = scope.nowIndex - weekNum / 4;
-            if (needChangeUpIndex < 0)
-              needChangeUpIndex = weekNum + needChangeUpIndex;
-
-            if (scope.weeks[getNextIndex(needChangeUpIndex)] == '' || scope.weeks[getNextIndex(needChangeUpIndex)] - 1 < min)
-              scope.weeks[needChangeUpIndex] = '';
-            else
-              scope.weeks[needChangeUpIndex] = scope.weeks[getNextIndex(needChangeUpIndex)] - 1;
-
-            var needChangeDownIndex = scope.nowIndex + weekNum / 4;
-            if (needChangeDownIndex > (weekNum - 1))
-              needChangeDownIndex = needChangeDownIndex - weekNum;
-
-            if (scope.weeks[getPrevIndex(needChangeDownIndex)] == '' || scope.weeks[getPrevIndex(needChangeDownIndex)] + 1 > max)
-              scope.weeks[needChangeDownIndex] = '';
-            else
-              scope.weeks[needChangeDownIndex] = scope.weeks[getPrevIndex(needChangeDownIndex)] + 1;
-          }
-
-
-          scope.weeks = [];
-          console.log(scope.listData[1]);
-          for (var i = 0; i < scope.listData.length; i++) {
-            scope.weeks.push(i + 1);
-          }
-
-          scope.getShowString = function (date) {
-            return date;
-          }
-          scope.getShowString = function (item) {
-            return scope.listData[item];
-          };
-
-          scope.onReleaseYear = function ($event) {
-
-          };
-        }
-
-        else {
-          function refreshData() {
-            scope.nowIndex = Math.round(scope.nowIndex);
-            if (scope.nowIndex > (weekNum - 1))
-              scope.nowIndex -= weekNum;
-            scope.year = scope.weeks[scope.nowIndex].getFullYear();
-            var needChangeUpIndex = scope.nowIndex - weekNum / 4;
-            if (needChangeUpIndex < 0)
-              needChangeUpIndex = weekNum + needChangeUpIndex;
-            var UpDate = new Date(scope.weeks[getNextIndex(needChangeUpIndex)]);
-            UpDate.setDate(UpDate.getDate() - 7);
-            scope.weeks[needChangeUpIndex] = UpDate;
-
-            var needChangeDownIndex = scope.nowIndex + weekNum / 4;
-            if (needChangeDownIndex > (weekNum - 1))
-              needChangeDownIndex = needChangeDownIndex - weekNum;
-            var DownDate = new Date(scope.weeks[getPrevIndex(needChangeDownIndex)]);
-            DownDate.setDate(DownDate.getDate() + 7);
-            scope.weeks[needChangeDownIndex] = DownDate;
-          }
-
-
-          scope.today = new Date();
-          scope.year = scope.today.getFullYear();
-          scope.chooseWeek = "";
-
-          function formatDate(value) {
-            if (parseInt(value) < 10) {
-              return '0' + value;
-            }
-            return value;
-          }
-
-          scope.getShowString = function (date) {
-            var day = date.getDay();
-            var weekStart = new Date(date);
-            weekStart.setDate(weekStart.getDate() - (day - 1));
-            var weekEnd = new Date(date);
-            weekEnd.setDate(weekEnd.getDate() + (7 - day));
-            return formatDate(weekStart.getMonth() + 1) + '月' + formatDate(weekStart.getDate()) + '日 ~ ' +
-              formatDate(weekEnd.getMonth() + 1) + '月' + formatDate(weekEnd.getDate()) + '日';
-          };
-
-          function initWeeks(year) {
-            scope.weeks = [];
-            var listDate = new Date();
-            listDate.setFullYear(year);
-            listDate.setDate(scope.today.getDate() - 6 * 7);
-            for (var i = 0; i < weekNum; i++) {
-              var tempDate = new Date(listDate);
-              scope.weeks[i] = tempDate;
-              listDate.setDate(listDate.getDate() + 7);
-            }
-          }
-
-          initWeeks(scope.today.getFullYear());
-
-          scope.onReleaseYear = function ($event) {
-            var slideLeft = $event.gesture.deltaX < 0;
-            if (slideLeft)
-              scope.year++;
-            else
-              scope.year--;
-            initWeeks(scope.year);
-          };
-
-          scope.selectItem = function (nowIndex) {
-            alert(scope.getShowString(scope.weeks[scope.nowIndex]));
-          };
-        }
-
-      }
-    }
-  });
