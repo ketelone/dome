f0ca7593232770bce1979a838d1ea6b4bd1f61eb
chenjiacheng <jiacheng.chen@hand-china.com>
2017-04-17 15:57:31 +0800

    'a'

---
+++ app/pages/my-info/my-room/myRoom.js
@@ -0,0 +1,39 @@
+/**
+ * Created by emillia on 2017/4/17.
+ */
+angular.module('myInfoModule')
+  .controller('myRoomCtrl', [
+    '$scope',
+    '$state',
+    'baseConfig',
+    '$ionicLoading',
+    '$http',
+    '$timeout',
+    '$ionicHistory',
+    '$ionicPlatform',
+    '$ionicScrollDelegate',
+    'hmsPopup',
+    '$rootScope',
+    'publicMethod',
+    '$stateParams','SettingsService','$translate',
+    function ($scope,
+              $state,
+              baseConfig,
+              $ionicLoading,
+              $http,
+              $timeout,
+              $ionicHistory,
+              $ionicPlatform,
+              $ionicScrollDelegate,
+              hmsPopup,
+              $rootScope, publicMethod,$stateParams,SettingsService,$translate) {
+
+
+
+
+
+
+
+
+    }
+]);
