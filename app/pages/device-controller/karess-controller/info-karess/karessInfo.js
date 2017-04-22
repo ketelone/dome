30f6c736c3de28d396973c9c6c87df9ca55ad588
代东东 <daidongdong@daidongdongdeMacBook-Pro.local>
2017-04-22 16:14:34 +0800

    karess

--- app/pages/device-controller/karess-controller/info-karess/karessInfo.js
+++ app/pages/device-controller/karess-controller/info-karess/karessInfo.js
@@ -14,6 +14,55 @@ angular.module('karessControlModule')
         $ionicHistory.goBack();
       }
 
+      function openLocation()
+      {
 
+        cordova.plugins.location.openLocation({"timeout":"3000"},success , error);
+        function success(msg)
+        {
+          console.log(msg);
+        }
+        // 返回值 {"latitude":"1231231","longitude":"1231231"}
+
+        function error(msg)
+        {
+          console.log(msg);
+        }
+      }
+
+      $scope.deciveInfo = {
+        id: "3",
+        deviceName: "Bathroom Header",
+        version: "1.2.6-133.0139",
+        sku: "1.2.6-133.0139",
+        sn: "1.2.6-133.0139",
+        place: "上海市闵行区1234567"
+      };
+
+      /**
+       *@autor: caolei
+       *@params: Object deciveInfo
+       *@disc: update device's place information
+       */
+      $scope.save = function(item){
+        //save device info
+        var url = baseConfig.basePath + "/dvm/deviceInfo/update";
+        var paramter = {
+          deviceId: item.id,
+          location: item.place
+        };
+        hmsHttp.post(url, paramter).success(
+          function(response){
+            console.log(response);
+            $scope.deciveInfo.place = response.rows[0].location;
+            hmsPopup.showPopup("<span translate='bathroom.saveAlert'></span>");
+          }
+        ).error(
+          function (response, status, header, config){
+            alert("-----");
+            hmsPopup.showPopup("<span translate='bathroom.saveError'></span>");
+          }
+        );
+      }
 
     }]);
--- app/pages/device-controller/karess-controller/karess.service.js
+++ app/pages/device-controller/karess-controller/karess.service.js
@@ -5,20 +5,142 @@ angular.module('karessControlModule')
   .service('karessService',
     ['baseConfig',
       function (baseConfig) {
-        var screenSize = {}
-        /**
-         *@autor:daidongdong
-         *@name:
-         *@params:
-         *@return:
-         *@disc:panduan kraess shifou anzhuang chajian
-         */
-        var cmdChajian = {}
-        this.chajian = function (result) {
-          screenSize = result;
+
+        function Karess() {
+        };
+        Karess.prototype._data = {
+          closeAll: "00",
+          openFiller: "2201",
+          closeFiller: "2200",
+          closeMassageBack: "2100",
+          openMassageBack: "2101",
+          closeMassagePillow: "2300",
+          openMassagePillow: "2301",
+          openSanitize: "2401",
+          closeSanitize: "2400",
+          openHeatBack: "2701",
+          closeHeatBack: "2700",
+          openDrain: "2501",
+          closeDrain: "2500",
+          _paramMassageBack: {
+            flow: {
+              LOW: "1",
+              HIGH: "5",
+              RESERVED: "0"
+            },
+            mode: {
+              NORMAL: "0"
+            }
+          },
+          _paramFiller: {
+            flow: {
+              LOW: "1",
+              LOW_MID: "2",
+              MID: "3",
+              MID_HIGH: "4",
+              HIGH: "5"
+            },
+            outlet: {
+              BATH_FAUCET: "1",
+              HANDSHOWER: "2",
+            }
+          },
+          _paramHeadBack: {
+            temp: {
+              LOW: "00",
+              HIGH: "01"
+            }
+          }
         };
 
-        this.chajian = function () {
-          return screenSize;
+        /**
+         * 设置水力按摩(massage-back)的按摩水压
+         * HIGH，LOW
+         */
+        Karess.prototype.setHeatParam = function (temp) {
+          return "07" + temp;
+        };
+
+        /**
+         * 设置背部加热参数
+         * HIGH，LOW
+         */
+        Karess.prototype.setMassageBackPressure = function (flow, mode) {
+          return "01" + flow + "00" + mode;
+        };
+
+        /**
+         * 设置注水(filler)的参数
+         * temp: 0-100
+         * level 0 - 100
+         * flow: _data._paramFiller.flow.*
+         * outlet _data._paramFiller.outlet.*
+         */
+        Karess.prototype.setFillerParams = function (temp, level, flow, outlet) {
+          var t = doStr(temp.toString(16));
+          var l = doStr(level.toString(16));
+          return "02" + t + l + outlet + flow;
+        };
+
+
+        function getHex(bin) {
+          var hex = "";
+          console.log(bin + "  " + bin.length)
+          for (var i = 0; i < bin.length; i += 4) {
+            var h = bin.substring(i, i + 4);
+            hex += parseInt(h, 2).toString(16).toUpperCase();
+          }
+          return hex;
         }
+
+
+        /**
+         * 返回cmd字段命令
+         * @param {*} header 头 16进制
+         * @param {*} idx 索引 16进制
+         * @param {*} data 数据段 16进制
+         * @param {*} ctrId 控制段 16进制
+         * @param {*} devId 设备段 16进制
+         */
+        function getCmd(header, idx, data, ctrId, devId) {
+          if (data.length % 2 != 0) {
+            data = "0" + data;
+          }
+          var checksum = parseInt(idx, 16) ^ parseInt(ctrId, 16) ^ parseInt(devId, 16);
+          for (var i = 0, len = data.length; i < len; i += 2) {
+            var hex = data.substring(i, i + 2);
+            checksum ^= parseInt(hex, 16);
+          }
+          var length = data.length / 2 + 4;
+          return header + doStr(length) + doStr(idx) + doStr(ctrId) + doStr(devId) + data + doStr(checksum.toString(16));
+        }
+
+        /**
+         * 十六进制补0
+         * @param {*字符} d
+         */
+        function doStr(d) {
+          if (d.length % 2 != 0) {
+            d = "0" + d;
+          }
+          return d;
+        }
+
+
+        /**
+         * 测试
+         */
+
+        var k = new Karess();
+        var allData = k._data.closeFiller;
+        console.log(getCmd("8877", 1, allData, 0, 5));  //关闭所有
+
+        var setFiller = k.setFillerParams(50, 50, k._data._paramFiller.flow.HIGH, k._data._paramFiller.outlet.BATH_FAUCET);
+        console.log(getCmd("8877", 1, setFiller, 0, 5));
+
+        var setHeatBack = k.setHeatParam(k._data._paramHeadBack.temp.HIGH);
+        console.log(getCmd("8877", 1, setHeatBack, 0, 5));
+
+        var setHeatBack = k.setMassageBackPressure(k._data._paramMassageBack.flow.HIGH, k._data._paramMassageBack.mode.NORMAL);
+        console.log(getCmd("8877", 1, setHeatBack, 0, 5));
       }]);
