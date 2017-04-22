b8c539a3b7c2472cd10370e7de64d8d8181cc6b9
代东东 <daidongdong@daidongdongdeMacBook-Pro.local>
2017-04-22 18:29:42 +0800

    plugin

---
+++ plugins/cordova.plugin.location/src/ios/CDVLocation.h
@@ -0,0 +1,13 @@
+//
+//  CDVLocation.h
+//  HelloCordova
+//
+//  Created by wangsheng on 2017/4/11.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface CDVLocation : CDVPlugin
+- (void)openLocation:(CDVInvokedUrlCommand *)command;
+@end
