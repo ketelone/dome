e5a24da5e48edc34504ba57b834c6284dea34c32
Angela <xuyi.liao@hand-china.com>
2017-05-16 18:36:20 +0800

    nextgen

--- app/pages/index-page/indexPage.js
+++ app/pages/index-page/indexPage.js
@@ -269,7 +269,7 @@ angular.module('indexPageModule')
             errorPictureUrl: "build/img/index/icon_home_device_warnning.png",
             isStatus: true,
             isError: false,
-            sku: "E8:91:E0:DC:20:F1"
+            sku: "K-99879T-7-CP"//"E8:91:E0:DC:20:F1"//F0:F0:87:F5:A2:17
           },
           {
             id: "8",
