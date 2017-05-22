d1c841331f905b4a4e6d687d326e04bb0b640ee4
10897 <cuiwang.zhao@hand-china.com>
2017-05-22 20:32:22 +0800

    z

--- app/pages/device-controller/mc-controller/mc-learning/mc.learning.js
+++ app/pages/device-controller/mc-controller/mc-learning/mc.learning.js
@@ -15,10 +15,30 @@ angular.module('mcControlModule')
              ) {
 
       if (window.localStorage.language == "default"||window.localStorage.language == '中文简体') {
-        $scope.image = 'build/img/mc-controller/jg.png';
+        $scope.images = ['build/img/mc-controller/mc-learning/jg1.png',
+          'build/img/mc-controller/mc-learning/jg2.png',
+          'build/img/mc-controller/mc-learning/jg3.png',
+          'build/img/mc-controller/mc-learning/jg4.png'];
+
       }else if(window.localStorage.language == 'English'){
-        $scope.image = 'build/img/mc-controller/jgen.png';
+        $scope.images = ['build/img/mc-controller/mc-learning/jge1.png',
+          'build/img/mc-controller/mc-learning/jge2.png',
+          'build/img/mc-controller/mc-learning/jge3.png',
+          'build/img/mc-controller/mc-learning/jge4.png'];
       }
+      $scope.index = 0;
+      $scope.image = $scope.images[0];
+
+      $scope.next = function () {
+        $scope.index += 1;
+        if($scope.index <4){
+          $scope.image = $scope.images[$scope.index];
+        }else{
+          $scope.index = 0;
+          $scope.image = $scope.images[0];
+        }
+      }
+
       $scope.goBack = function () {
         publicMethod.goBack();
       };
--- app/pages/device-controller/toilet-controller/toilet-learning/toilet.learning.js
+++ app/pages/device-controller/toilet-controller/toilet-learning/toilet.learning.js
@@ -26,19 +26,27 @@ angular.module('toiletControlModule')
       $scope.goBack = function () {
         publicMethod.goBack();
       };
-      $scope.chongshuisetval = false,
-      $scope.chuchousetval = false,
+      $scope.chongshuisetval = false;
+      $scope.chuchousetval = false;
       // $scope.anjianvoicesetval = true,
       // $scope.welcomemsetval = false
+      if (window.localStorage.language == "default"||window.localStorage.language == '中文简体') {
       $scope.images = ['build/img/toilet-controller/toilet-learning/mt1.png',
                         'build/img/toilet-controller/toilet-learning/mt2.png',
-                        'build/img/toilet-controller/toilet-learning/mt3.png',];
+                        'build/img/toilet-controller/toilet-learning/mt3.png',
+                        'build/img/toilet-controller/toilet-learning/mt4.png'];
+      }else if(window.localStorage.language == 'English'){
+        $scope.images = ['build/img/toilet-controller/toilet-learning/mte1.png',
+          'build/img/toilet-controller/toilet-learning/mte2.png',
+          'build/img/toilet-controller/toilet-learning/mte3.png',
+          'build/img/toilet-controller/toilet-learning/mte4.png'];
+      }
       $scope.index = 0;
       $scope.image = $scope.images[0];
 
       $scope.next = function () {
         $scope.index += 1;
-        if($scope.index <3){
+        if($scope.index <4){
           $scope.image = $scope.images[$scope.index];
         }else{
           $scope.index = 0;
