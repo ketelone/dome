6fb6af890111333000742e0a7321d77548250a48
Angela <xuyi.liao@hand-china.com>
2017-05-26 10:39:21 +0800

    message

--- app/pages/message/message-detail/messageDetail.js
+++ app/pages/message/message-detail/messageDetail.js
@@ -1,94 +1,68 @@
-/**
- * Created by chenjiacheng on 2017/3/28.
- */
 angular.module('messageModule')
-
   .controller('messageDetailCtrl', [
-    '$scope',
-    '$state',
-    'baseConfig',
-    '$ionicLoading',
-    '$http',
-    '$timeout',
-    '$ionicHistory',
-    '$ionicPlatform',
-    '$ionicScrollDelegate',
-    'hmsPopup',
-    '$rootScope',
-    'publicMethod',
-    '$stateParams','hmsHttp','SettingsService','baseConfig',
-    function ($scope,
-              $state,
-              baseConfig,
-              $ionicLoading,
-              $http,
-              $timeout,
-              $ionicHistory,
-              $ionicPlatform,
-              $ionicScrollDelegate,
-              hmsPopup,
-              $rootScope, publicMethod,$stateParams,hmsHttp,SettingsService,baseConfig) {
+    '$scope', '$state', 'baseConfig',
+    '$ionicLoading', '$http', '$timeout',
+    '$ionicHistory', '$ionicPlatform',
+    '$ionicScrollDelegate', 'hmsPopup',
+    '$rootScope', 'publicMethod',
+    '$stateParams', 'hmsHttp',
+    'SettingsService', 'baseConfig',
+    function ($scope, $state, baseConfig, $ionicLoading, $http,
+              $timeout, $ionicHistory, $ionicPlatform,
+              $ionicScrollDelegate, hmsPopup,
+              $rootScope, publicMethod, $stateParams,
+              hmsHttp, SettingsService, baseConfig) {
 
-   $scope.messageDetail="";
-   $scope.hasUpdate="";
-      $scope.goBack = function(){
+      $scope.goBack = function () {
         $ionicHistory.goBack();
       }
 
-  var exceptionId=SettingsService.get("exceptionId");
-  // console.log(exceptionId+1);
-      function hasRead(){
-        var url = baseConfig.basePath+"/r/api/cmm/deviceException/update";
+      $scope.messageDetail = "";
+      $scope.hasUpdate = "";
+
+      var exceptionId = SettingsService.get("exceptionId");
+      // console.log(exceptionId+1);
+      function hasRead() {
+        var url = baseConfig.basePath + "/r/api/cmm/deviceException/update";
         var paramter =
-          {"exceptionId":exceptionId,"processStatus":"Y"};
+          {"exceptionId": exceptionId, "processStatus": "Y"};
         hmsHttp.post(url, paramter).success(
-          function(response){
-          //  alert("success");
+          function (response) {
+            //  alert("success");
             alert(response);
-            $scope.hasUpdate=response;
-  }
+            $scope.hasUpdate = response;
+          }
         ).error(
-          function (response, status, header, config){
+          function (response, status, header, config) {
             //hmsPopup.showPopup("<span translate='bathroom.saveError'></span>");
             //  alert("1234");
           }
         );
 
       }
+
       hasRead();
 
 
-      function displayDetail(){
-        var url = baseConfig.basePath+"/r/api/cmm/exceptionDetail/query";
+      function displayDetail() {
+        var url = baseConfig.basePath + "/r/api/cmm/exceptionDetail/query";
         var paramter = [{}];
         hmsHttp.post(url, paramter).success(
-          function(response){
-          //  alert("success");
-           // alert(response);
-            $scope.messageDetail=response;
+          function (response) {
+            //  alert("success");
+            // alert(response);
+            $scope.messageDetail = response;
           }
         ).error(
-          function (response, status, header, config){
+          function (response, status, header, config) {
             //hmsPopup.showPopup("<span translate='bathroom.saveError'></span>");
             //  alert("1234");
           }
         );
 
       }
+
       displayDetail();
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }]);
--- app/pages/message/message.js
+++ app/pages/message/message.js
@@ -1,13 +1,13 @@
 /**
- * Created by chenjiacheng on 17/3/27.
+ * Writed by LiaoXuyi
  */
-
 angular.module('messageModule')
   .config(['$stateProvider',
     function ($stateProvider) {
       //配置消息详情页面路由
+      //Configure messageDetail router
       $stateProvider.state('messageDetail', {
-        url: '/messageDetail',
+        url: '/message/detail/:messageId',
         templateUrl: 'build/pages/message/message-detail/messageDetail.html',
         controller: 'messageDetailCtrl'
       })
@@ -18,16 +18,25 @@ angular.module('messageModule')
     'hmsPopup', 'hmsHttp', 'SettingsService', 'baseConfig',
     function ($scope, $state, $timeout, publicMethod, $ionicPopup,
               hmsPopup, hmsHttp, SettingsService, baseConfig) {
-      $scope.data = {
-        showDelete: false //左侧选择框是否显示
-      };
+      /**whether show delete checkbox on the left**/
+      $scope.showDelete=false //左侧选择框是否显示
+      /**whether show three button on the bottom**/
       $scope.threeBottom = false;//底下三个选项是否显示
+      /**the message object array**/
       $scope.messages = [];//消息对象数组
+      /**the message to tell user the messages array is empty**/
       $scope.empty_message="";//空消息的内容
+      /**whether show empty_message**/
       $scope.showEmptyMessage=false;//是否显示空消息
+      /**the clicked tab **/
       $scope.index=0;//选中的tab的下标
 
       //设置messages数组的数据
+      /**
+       * set type of message and ask the interface to get the messages
+       * @param type:the type of message
+       * @return the new messages array
+       */
       function setMessages(type) {
         $scope.messages=[];
         if(type==0) {
@@ -108,12 +117,18 @@ angular.module('messageModule')
       }
 
       //初始化messages数组
+      /**init the messages array when entering the message page**/
       setMessages(0);
 
       //tab点击，切换内容
+      /**
+       * change the clicked tab style and reset messages array
+       * if messages array is empty,show the empty_message
+       * @param index: the index of clicked tab
+       */
       $scope.listClick = function (index) {
         $scope.index=index;
-        setMessages(index);
+        setMessages(index);//获取数据
         if($scope.messages.length==0){
           $scope.empty_message="message.noMessageWord"+index;
           $scope.showEmptyMessage=true;
@@ -123,64 +138,44 @@ angular.module('messageModule')
         }
       }
 
+      /**the img of selected checkbox**/
       var circleUrl2 = "build/img/common/radio_h.png";
+      /**the img of unselected checkbox**/
       var circleUrltemp = "build/img/common/radio_q.png";
 
       /**
-       *@author:chenjiacheng
-       *@name:logout
-       *@params:
-       *@return:
-       *@disc:go  messageDetail list
-       */
-      $scope.goMessDetail = function (item) {
-        SettingsService.set("exceptionId", item.id);
-        $state.go("messageDetail");
-      }
-
-      /**
-       *@author:chenjiacheng
-       *@name:goDetele
-       *@params:
-       *@return:
-       *@disc:goDetele item
+       * delete message and refresh the messages arrays
+       * @param item:the clicked message
        */
       $scope.goDeteleitem = function (item) {
-        console.log(item);
         var toDetele = function (res) {
-          if(res==true){
+          if(res==true){//comfirm to delete message
+            //ask the interface to delete the message
+            // var paramter = [{"exceptionId": item.id}];
+            // console.log(paramter);
+            // var url = baseConfig.basePath + "/r/api/cmm/deviceException/delete";
+            // hmsHttp.post(url, paramter).success(function (response) {
+            //   console.log(response);
+            // }).error(
+            // );
+            //refresh the messages arrays
             $scope.messages.splice($scope.messages.indexOf(item), 1);
           }
-          // var paramter = [{"exceptionId": item.id}];
-          // console.log(paramter);
-          // var url = baseConfig.basePath + "/r/api/cmm/deviceException/delete";
-          // hmsHttp.post(url, paramter).success(function (response) {
-          //   console.log(response);
-          // }).error(
-          // );
         };
+        //pop the dialog to ask whether confirm to delete message
         hmsPopup.confirmNoTitle("<div style='text-align:center'>删除后将无法在消息记录中找回,<br/>是否要删除此消息?</div>",
         "删除","取消",toDetele);
       };
 
-      /**
-       *@author:chenjiacheng
-       *@name:manyChoose
-       *@params:
-       *@return:
-       *@disc:ClickmanyChoose
-       */
+      /**show three buttom on the bottom and checkbox on the left**/
       $scope.manyChoose = function () {
         $scope.threeBottom = true;
-        $scope.data.showDelete = true;
+        $scope.showDelete = true;
       }
 
       /**
-       *@author:chenjiacheng
-       *@name:onChoose
-       *@params:
-       *@return:
-       *@disc:choose you click
+       * reverse the img of checkbox when the message is selected
+       * @param item:the clicked message
        */
       $scope.onChoose = function (item) {
         if (item.ischecked) {//被选中
@@ -191,43 +186,26 @@ angular.module('messageModule')
         item.ischecked=!item.ischecked;
       }
 
-      /**
-       *@author:chenjiacheng
-       *@name:bottomGocancel
-       *@params:
-       *@return:
-       *@disc:clickbottomGocancel
-       */
+      /**cancel the multi-choose operation,hide three button and checkbox**/
       $scope.bottomGocancel = function () {
         $scope.messages.forEach(function (message) {
           message.ischecked = false;
           message.circleUrl1 = circleUrltemp;
         });
         $scope.threeBottom = false;
-        $scope.data.showDelete = false;
+        $scope.showDelete = false;
       }
 
-      /**
-       *@author:chenjiacheng
-       *@name:bottomManychoose
-       *@params:
-       *@return:
-       *@disc:clickbottomManychoose
-       */
+      /**select all message in this tab**/
       $scope.bottomManychoose = function () {
+        //change the img of checkbox
         $scope.messages.forEach(function (message) {
           message.ischecked = true;
           message.circleUrl1 = circleUrl2;
         });
       }
 
-      /**
-       *@author:chenjiacheng
-       *@name:bottomGodetele
-       *@params:
-       *@return:
-       *@disc:clickbottomGodetele
-       */
+      /**delete the selected messages**/
       $scope.bottomGodetele = function () {
         var deleteArray=$scope.messages.filter(function(message){
           return message.ischecked;
@@ -236,21 +214,24 @@ angular.module('messageModule')
           return !message.ischecked;
         });
         var toDetele = function (res) {
-          if(res==true){
+          if(res==true){//comfirm to delete message
+            //refresh the messages arrays
             $scope.messages=tempArray;
+            // var paramter = [{"exceptionId": item.id}];
+            // console.log(paramter);
+            // var url = baseConfig.basePath + "/r/api/cmm/deviceException/delete";
+            // hmsHttp.post(url, paramter).success(function (response) {
+            //   console.log(response);
+            // }).error(
+            // );
           }
-          // var paramter = [{"exceptionId": item.id}];
-          // console.log(paramter);
-          // var url = baseConfig.basePath + "/r/api/cmm/deviceException/delete";
-          // hmsHttp.post(url, paramter).success(function (response) {
-          //   console.log(response);
-          // }).error(
-          // );
         };
+        //pop the dialog to ask whether confirm to delete messages
         hmsPopup.confirmNoTitle("<div style='text-align:center'>删除后将无法在消息记录中找回,<br/>是否要删除这些消息?</div>",
           "删除","取消",toDetele);
+        //hide three button and checkbox
         $scope.threeBottom = false;
-        $scope.data.showDelete = false;
+        $scope.showDelete = false;
       }
 
     }]);
