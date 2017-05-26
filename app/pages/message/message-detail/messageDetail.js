1da4018f677a7af93f22362a8842301930c09f1e
Angela <xuyi.liao@hand-china.com>
2017-05-26 19:57:45 +0800

    message-detail

--- app/pages/message/message-detail/messageDetail.js
+++ app/pages/message/message-detail/messageDetail.js
@@ -13,56 +13,73 @@ angular.module('messageModule')
               $rootScope, publicMethod, $stateParams,
               hmsHttp, SettingsService, baseConfig) {
 
+      /**go back to last url**/
       $scope.goBack = function () {
         $ionicHistory.goBack();
       }
 
-      $scope.messageDetail = "";
-      $scope.hasUpdate = "";
+      /**message's id**/
+      var messageId=$stateParams.messageId;
 
-      var exceptionId = SettingsService.get("exceptionId");
-      // console.log(exceptionId+1);
+      /**message's detail**/
+      $scope.data={};
+
+      $scope.data={
+        "box":"BOX1",
+        "room":"卫生间",
+        "device":"马桶",
+        "description":"进水滤芯<br/>快到使用期限，请及时更换",
+        "hasRead":false,
+        "advice":"滤芯需更换？快去商城购买吧！",
+        "link":""
+      };
+
+      /**display message's detail**/
+      // var displayDetail=function () {
+      //   var url = baseConfig.basePath + "/r/api/cmm/exceptionDetail/query";
+      //   var paramter = [{"exceptionId": messageId}];
+      //   hmsHttp.post(url, paramter).success(
+      //     function (response) {
+      //       //  alert("success");
+      //       // alert(response);
+      //       if(response.success == true) {
+      //         $scope.data={
+      //           "box":response.box,
+      //           "room":response.room,
+      //           "device":response.device,
+      //           "description":response.description,
+      //           "hasRead":response.hasRead,
+      //           "advice":response.advice,
+      //           "link":response.link
+      //         };
+      //       }
+      //     }
+      //   ).error(
+      //     function (response, status, header, config) {
+      //       //hmsPopup.showPopup("<span translate='bathroom.saveError'></span>");
+      //       //  alert("1234");
+      //     }
+      //   );
+      // };
+
+      /**change the message's read status**/
       function hasRead() {
-        var url = baseConfig.basePath + "/r/api/cmm/deviceException/update";
-        var paramter =
-          {"exceptionId": exceptionId, "processStatus": "Y"};
-        hmsHttp.post(url, paramter).success(
-          function (response) {
-            //  alert("success");
-            alert(response);
-            $scope.hasUpdate = response;
-          }
-        ).error(
-          function (response, status, header, config) {
-            //hmsPopup.showPopup("<span translate='bathroom.saveError'></span>");
-            //  alert("1234");
-          }
-        );
-
+        $scope.data.hasRead = true;
+        // var url = baseConfig.basePath + "/r/api/cmm/deviceException/update";
+        // var paramter =
+        //   {"exceptionId": exceptionId, "processStatus": "Y"};
+        // hmsHttp.post(url, paramter).success(
+        //   function (response) {
+        //     //  alert("success");
+        //     alert(response);
+        //     $scope.data.hasRead = true;
+        //   }
+        // ).error(
+        //   function (response, status, header, config) {
+        //     //hmsPopup.showPopup("<span translate='bathroom.saveError'></span>");
+        //     //  alert("1234");
+        //   }
+        // );
       }
-
-      hasRead();
-
-
-      function displayDetail() {
-        var url = baseConfig.basePath + "/r/api/cmm/exceptionDetail/query";
-        var paramter = [{}];
-        hmsHttp.post(url, paramter).success(
-          function (response) {
-            //  alert("success");
-            // alert(response);
-            $scope.messageDetail = response;
-          }
-        ).error(
-          function (response, status, header, config) {
-            //hmsPopup.showPopup("<span translate='bathroom.saveError'></span>");
-            //  alert("1234");
-          }
-        );
-
-      }
-
-      displayDetail();
-
 
     }]);
