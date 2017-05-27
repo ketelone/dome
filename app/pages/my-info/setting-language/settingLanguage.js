a195b143cf6fe431908916050b4ad20c95db468b
Angela <xuyi.liao@hand-china.com>
2017-05-27 11:26:49 +0800

    language

--- app/pages/my-info/setting-language/settingLanguage.js
+++ app/pages/my-info/setting-language/settingLanguage.js
@@ -1,57 +1,38 @@
-/**
- * Created by chenjiacheng on 2017/3/28.
- */
-//myInfoModule$translateProvider', '$translateStaticFilesLoaderProvider'
-//.config([
-//'$translateProvider', '$translateStaticFilesLoaderProvider',
-//  function (  $translateProvider, $translateStaticFilesLoaderProvider) {
-
-
-  // }])
-
 angular.module('myInfoModule')
   .controller('settingLanguageCtrl', [
-    '$scope',
-    '$state',
-    'baseConfig',
-    '$ionicLoading',
-    '$http',
-    '$timeout',
-    '$ionicHistory',
-    '$ionicPlatform',
-    '$ionicScrollDelegate',
-    'hmsPopup',
-    '$rootScope',
-    'publicMethod',
-    '$stateParams', 'SettingsService', '$translate',
-    function ($scope,
-              $state,
-              baseConfig,
-              $ionicLoading,
-              $http,
-              $timeout,
-              $ionicHistory,
-              $ionicPlatform,
-              $ionicScrollDelegate,
-              hmsPopup,
-              $rootScope, publicMethod, $stateParams, SettingsService, $translate) {
+    '$scope','$state','baseConfig','$ionicLoading','$http',
+    '$timeout','$ionicHistory','$ionicPlatform',
+    '$ionicScrollDelegate','hmsPopup','$rootScope',
+    'publicMethod','$stateParams', 'SettingsService', '$translate',
+    function ($scope,$state,baseConfig,$ionicLoading,
+              $http,$timeout,$ionicHistory,
+              $ionicPlatform,$ionicScrollDelegate,
+              hmsPopup,$rootScope, publicMethod, $stateParams,
+              SettingsService, $translate) {
 
-
-      $scope.languageItems = [{
-        ischecked: true,
-        language: "my-info.setting-language.followLanguage",
-        radioImg1: "build/img/common/radio_q.png",
-        radioImg2: "build/img/common/radio_h.png",
-        radioTemp: "build/img/common/radio_q.png"
-      }, {
-        ischecked: false,
-        language: "中文简体",
-        radioImg1: "build/img/common/radio_q.png",
-        radioImg2: "build/img/common/radio_h.png",
-        radioTemp: "build/img/common/radio_q.png"
-      },
+      $scope.languageItems = [
+        {
+          ischecked: true,
+          languageId:undefined,
+          json_file: null,
+          language: "my-info.setting-language.followLanguage",
+          radioImg1: "build/img/common/radio_q.png",
+          radioImg2: "build/img/common/radio_h.png",
+          radioTemp: "build/img/common/radio_q.png"
+        },
         {
           ischecked: false,
+          languageId:"1",
+          json_file: "zh",
+          language: "中文简体",
+          radioImg1: "build/img/common/radio_q.png",
+          radioImg2: "build/img/common/radio_h.png",
+          radioTemp: "build/img/common/radio_q.png"
+        },
+        {
+          ischecked: false,
+          languageId:"2",
+          json_file: "tw",
           language: "中文繁体",
           radioImg1: "build/img/common/radio_q.png",
           radioImg2: "build/img/common/radio_h.png",
@@ -59,20 +40,17 @@ angular.module('myInfoModule')
         },
         {
           ischecked: false,
+          languageId:"0",
+          json_file: "en",
           language: "English",
           radioImg1: "build/img/common/radio_q.png",
           radioImg2: "build/img/common/radio_h.png",
           radioTemp: "build/img/common/radio_q.png"
         },
-        // {
-        //   ischecked: false,
-        //   language: "한국어",
-        //   radioImg1: "build/img/common/radio_q.png",
-        //   radioImg2: "build/img/common/radio_h.png",
-        //   radioTemp: "build/img/common/radio_q.png"
-        // },
         {
           ischecked: false,
+          languageId:"3",
+          json_file: "th",
           language: "ภาษาไทย",
           radioImg1: "build/img/common/radio_q.png",
           radioImg2: "build/img/common/radio_h.png",
@@ -88,30 +66,27 @@ angular.module('myInfoModule')
 
        */
       $scope.initLanguage = function () {
-        if (window.localStorage.language == "default") {
+        if (window.localStorage.languageId == undefined) {
           $scope.languageItems[0].ischecked = true;
           $scope.languageItems[0].radioImg1 = "build/img/common/radio_h.png";
-
         }
-        if (window.localStorage.language == '中文简体') {
+        if (window.localStorage.languageId == "1") {
           $scope.languageItems[1].ischecked = true;
           $scope.languageItems[1].radioImg1 = "build/img/common/radio_h.png";
 
         }
-        if (window.localStorage.language == '中文繁体') {
+        if (window.localStorage.languageId == '2') {
           $scope.languageItems[2].ischecked = true;
           $scope.languageItems[2].radioImg1 = "build/img/common/radio_h.png";
         }
-        if (window.localStorage.language == 'English') {
+        if (window.localStorage.languageId == '0') {
           $scope.languageItems[3].ischecked = true;
           $scope.languageItems[3].radioImg1 = "build/img/common/radio_h.png";
         }
-        if (window.localStorage.language == 'ภาษาไทย') {
+        if (window.localStorage.languageId == '3') {
           $scope.languageItems[4].ischecked = true;
           $scope.languageItems[4].radioImg1 = "build/img/common/radio_h.png";
         }
-
-
       }
       $scope.initLanguage();
 
@@ -136,68 +111,40 @@ angular.module('myInfoModule')
       $scope.chooseLanguage = function (item) {
         item.ischecked = true;
         item.radioImg1 = "build/img/common/radio_h.png";
-        window.localStorage.language = item.language;
-
-
+        window.localStorage.languageId = item.languageId;
         for (var i = 0; i < $scope.languageItems.length; i++) {
           if ($scope.languageItems[i].language != item.language) {
             $scope.languageItems[i].ischecked = false;
             $scope.languageItems[i].radioImg1 = "build/img/common/radio_q.png";
           }
         }
-
-        if (item.language == 'my-info.setting-language.followLanguage') {
-
-          window.localStorage.language = "default";
+        if (item.languageId == undefined) {
           navigator.globalization.getPreferredLanguage(
             function (language) {
-
               if (language.value == 'zh-CN' || language.value == 'zh-Hans-CN') {
                 $translate.use('zh');
-
               }
               else if (language.value == 'zh-TW' || language.value == 'zh-Hans-TW') {
                 $translate.use('tw');
-
               }
               else if (language.value == 'en-US' || language.value == 'en-CN') {
                 $translate.use('en');
               }
               else if (language.value == 'en-TH' || language.value == 'th-CN') {
                 $translate.use('th');
-
               }
-
-
+              else {
+                $translate.use('en');
+              }
             },
             function () {
-
+              // alert('Error getting locale\n');
             });
-          publicMethod.goBack();
-        }
-        if (item.language == '中文简体') {
-
-          $translate.use('zh');
-
-          $ionicHistory.goBack();
-        }
-        else if (item.language == '中文繁体') {
-
-          $translate.use('tw');
-          $ionicHistory.goBack();
-        }
-        else if (item.language == 'English') {
-
-          $translate.use('en');
-          $ionicHistory.goBack();
-
         }
         else {
-          $translate.use('en');
-          $ionicHistory.goBack();
-          // publicMethod.goBack();
+          $translate.use(item.json_file);
         }
-
+        $ionicHistory.goBack();
 
         //if($(e.target).text().trim()=="中文简体"){
         //
--- app/pages/my-info/setting/setting.js
+++ app/pages/my-info/setting/setting.js
@@ -16,7 +16,7 @@ angular.module('myInfoModule')
     'hmsPopup',
     '$rootScope',
     'publicMethod',
-    '$stateParams','checkVersionService','SettingsService','$translate',
+    '$stateParams', 'checkVersionService', 'SettingsService', '$translate',
     function ($scope,
               $state,
               baseConfig,
@@ -27,7 +27,7 @@ angular.module('myInfoModule')
               $ionicPlatform,
               $ionicScrollDelegate,
               hmsPopup,
-              $rootScope, publicMethod,$stateParams,checkVersionService,SettingsService,$translate){
+              $rootScope, publicMethod, $stateParams, checkVersionService, SettingsService, $translate) {
 
       /**
        *@author:chenjiacheng
@@ -37,12 +37,12 @@ angular.module('myInfoModule')
        *@disc:Set goBack
 
        */
-      $scope.goBack = function(){
+      $scope.goBack = function () {
         $ionicHistory.goBack();
       }
 
 
- $scope.thisLanguage="";
+      $scope.thisLanguage = "";
 
       /**
        *@author:chenjiacheng
@@ -52,62 +52,61 @@ angular.module('myInfoModule')
        *@disc:display currentLanguage
 
        */
-$scope.currentLanguage=function(){
+      $scope.currentLanguage = function () {
 
-  if (window.localStorage.language == '中文简体') {
-    $scope.thisLanguage="中文简体";
-  }
-  else if(window.localStorage.language =="default"){
-    $scope.thisLanguage="my-info.setting.followLanguage";
+        if (window.localStorage.languageId == '1') {
+          $scope.thisLanguage = "中文简体";
+        }
+        else if (window.localStorage.languageId == undefined) {
+          $scope.thisLanguage = "my-info.setting.followLanguage";
 
-    //navigator.globalization.getPreferredLanguage(
-    //  function (language) {
+          //navigator.globalization.getPreferredLanguage(
+          //  function (language) {
 
-      //  if (language.value == 'zh-CN' ||language.value ==  'zh-Hans-CN') {
-      //    $scope.thisLanguage="中文简体";
-      //
-      //  }
-      //  else if (language.value == 'zh-TW' || language.value == 'zh-Hans-TW') {
-      //    $scope.thisLanguage="中文繁体";
-      //
-      //  }
-      //  else if (language.value == 'en-US' || language.value == 'en-CN') {
-      //    $scope.thisLanguage="English";
-      //  }
-      //  else if (language.value == 'en-TH' || language.value == 'th-CN') {
-      //    $translate.use('th');
-      //    $scope.thisLanguage="ภาษาไทย";
-      //  }
-      // else{
-      //    $scope.thisLanguage="English";
-      //  }
-      //},
-      //function () {
-      //
-      //});
-  }
-  else  if (window.localStorage.language == '中文繁体') {
-    $scope.thisLanguage="中文繁体";
-  }
-  else if (window.localStorage.language == 'English') {
-    $scope.thisLanguage="English";
-  }
-  else  if (window.localStorage.language == 'ภาษาไทย') {
-    $scope.thisLanguage="ภาษาไทย";
-  }
-  else{
-    $scope.thisLanguage="English";
-  }
+          //  if (language.value == 'zh-CN' ||language.value ==  'zh-Hans-CN') {
+          //    $scope.thisLanguage="中文简体";
+          //
+          //  }
+          //  else if (language.value == 'zh-TW' || language.value == 'zh-Hans-TW') {
+          //    $scope.thisLanguage="中文繁体";
+          //
+          //  }
+          //  else if (language.value == 'en-US' || language.value == 'en-CN') {
+          //    $scope.thisLanguage="English";
+          //  }
+          //  else if (language.value == 'en-TH' || language.value == 'th-CN') {
+          //    $translate.use('th');
+          //    $scope.thisLanguage="ภาษาไทย";
+          //  }
+          // else{
+          //    $scope.thisLanguage="English";
+          //  }
+          //},
+          //function () {
+          //
+          //});
+        }
+        else if (window.localStorage.language == '2') {
+          $scope.thisLanguage = "中文繁体";
+        }
+        else if (window.localStorage.language == '0') {
+          $scope.thisLanguage = "English";
+        }
+        else if (window.localStorage.language == '3') {
+          $scope.thisLanguage = "ภาษาไทย";
+        }
+        else {
+          $scope.thisLanguage = "English";
+        }
 
 
- }
+      }
 
       $scope.currentLanguage();
 //);
 
 
-
-  //}
+      //}
 
 
 //}
@@ -119,45 +118,39 @@ $scope.currentLanguage=function(){
        *@return:
        *@disc:Set default temperature
        */
-/*$scope.temperatureUnit=function()
-     {
-        if (SettingsService.get("temperature") != false) {
+      /*$scope.temperatureUnit=function()
+       {
+       if (SettingsService.get("temperature") != false) {
 
-         //console.log(SettingsService.get("temperature")+'1a');
-          return SettingsService.get("temperature");
-        }
-        else {
-         var  temperature=angular.element('#temperatureUnit').text();
-          SettingsService.set("temperature", temperature);
-          window.localStorage.temperature = "°C";
-          return SettingsService.get("temperature");
-        }
-
-
-    }*/
+       //console.log(SettingsService.get("temperature")+'1a');
+       return SettingsService.get("temperature");
+       }
+       else {
+       var  temperature=angular.element('#temperatureUnit').text();
+       SettingsService.set("temperature", temperature);
+       window.localStorage.temperature = "°C";
+       return SettingsService.get("temperature");
+       }
 
 
-/**
+       }*/
+
+
+      /**
        *@author:chenjiacheng
        *@name:logout
        *@params:
        *@return:
        *@disc:logout
        */
-      $scope.logout=function() {
+      $scope.logout = function () {
 
-        var goLogin=function(){
+        var goLogin = function () {
           $state.go('login');
         }
 
-        hmsPopup.confirmNoTitle( "<div style='text-align: center' >是否退出当前账号</div>",goLogin);
+        hmsPopup.confirmNoTitle("<div style='text-align: center' >是否退出当前账号</div>", goLogin);
       }
-
-
-
-
-
-
 
 
     }]);
